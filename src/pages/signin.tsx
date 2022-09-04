b99a5bd9071db88d5ca55888e44a76abd73d004a
Om Budhiraja <ombudhiraja144@gmail.com>
2022-09-05 02:13:43 +0530

    signup page upgraded

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import FullScreenLoader from '@/components/shared/FullScreenLoader';
 import Header from '@/components/shared/Header';
 import type { NextPage } from 'next';
@@ -27,14 +28,32 @@ const SignInPage: NextPage = () => {
       </Head>
       <div className="min-h-screen flex flex-col">
         <Header withUser={false} unlinkLogo={true} />
-        <section className="flex-1 flex flex-col items-center justify-center gap-3">
-          <h1 className="text-3xl font-semibold text-gray-700 mb-3">Sign In</h1>
-          <button className="btn block" onClick={() => signIn('github', { callbackUrl: '/' })}>
-            Sign In With Github
-          </button>
-          <button className="btn block" onClick={() => signIn('google', { callbackUrl: '/' })}>
-            Sign In With Google
-          </button>
+        <section className="flex-1 flex flex-col sm:flex-row mt-10 items-center justify-between gap-3">
+          <div className="flex-1 flex flex-col items-center justify-center gap-5 px-5 py-0">
+            <h1 className="text-6xl text-center font-semibold leading-tight">
+              Split the bill{' '}
+              <span className="text-[#261f82]">
+                {' '}
+                <br /> not the friendship
+              </span>
+            </h1>
+
+            <p className="text-lg text-gray-600">
+              Manage your Bills with Friends Effortlessly
+              <br />
+              Sign Up to access the App
+            </p>
+
+            <button
+              className="btn btn-primary min-w-[10rem]"
+              onClick={() => signIn(undefined, { callbackUrl: '/' })}
+            >
+              Get Started
+            </button>
+          </div>
+          <div className="flex-1 px-10 sm:px-0 max-w-sm sm:max-w-none">
+            <img className="h-full w-full bg-contain" src="/app-demo.svg" alt="app-demo" />
+          </div>
         </section>
       </div>
     </>
